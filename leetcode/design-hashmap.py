3ac5fcb63c433c4c2f4e25309890f758617ce7f3
Saai Arora <94809929+Saai151@users.noreply.github.com>
2025-08-20 09:45:09 -0400

    Add solution for design-hashmap

---
+++ leetcode/design-hashmap.py
@@ -0,0 +1,59 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashMap:
+    def __init__(self):
+        self.arr = [ListNode()] * 1000
+        
+
+    def put(self, key: int, value: int) -> None:
+        index = self.hash(key)
+
+        curr = self.arr[index]
+
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.val = value
+                return
+            curr = curr.next
+        
+        curr.next = ListNode(key, value)
+        
+        
+
+    def get(self, key: int) -> int:
+        index = self.hash(key)
+        curr = self.arr[index].next
+
+        while curr:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = self.hash(key)
+        curr = self.arr[index]
+
+        while curr and curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+        return -1
+
+
+    def hash(self, key):
+        index = key % len(self.arr)
+        return index
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
